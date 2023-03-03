eccf27061e0b23981047230704179a7241059fbd
Fernando Olguin <100617517+ferOlguiin@users.noreply.github.com>
2023-03-02 22:11:03 -0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || process.env.API_PORT;
 export const URI_MONGODB = process.env.URI_MONGODB;
 
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
